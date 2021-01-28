5b3ec91a4dcb27767d6eddbb841f662b53a8b071
Numchik <maksim.nikitjuk@icloud.com>
2021-01-29 00:41:18 +0500

    Initial Commit

---
+++ HW2.2Collor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.2Collor
+//
+//  Created by Максим Никитюк on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
