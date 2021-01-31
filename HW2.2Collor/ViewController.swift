9664b9b635bbc586c2ff22e7eaf23eb50f00adc5
Numchik <maksim.nikitjuk@icloud.com>
2021-02-01 02:20:58 +0500

    placedModules

--- HW2.2Collor/ViewController.swift
+++ HW2.2Collor/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var lableBlue: UILabel!
+    @IBOutlet var lableRedOne: UILabel!
+    @IBOutlet var lableGreenOne: UILabel!
+    @IBOutlet var lableBlueOne: UILabel!
+    @IBOutlet var viewCollour: UIView!
+    @IBOutlet var slaiderRed: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+    
     }
 
 
